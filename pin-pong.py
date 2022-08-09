dd9609636f23cad0e6801c1abe624279c1165a64
DIO <110611144+1emonc4ik@users.noreply.github.com>
2022-08-09 19:02:06 +0500

    Update pin-pong.py
    v1.1

--- pin-pong.py
+++ pin-pong.py
@@ -2,7 +2,7 @@ from pygame import *
 from random import randint
 from time import time as timer
 
-window = display.set_mode((1200, 720))
+window = display.set_mode((720, 500))
 display.set_caption('Ping-Pong')
 background = transform.scale(image.load('bckg.jpg'), (1200, 720))
 
@@ -20,10 +20,11 @@ font1 = font.SysFont('Arial', 40)
 lost = 0
 
 class GameSprite(sprite.Sprite):
-    def __init__(self, player_image, player_y, width, height, speed):
+    def __init__(self, player_image, player_x, player_y, width, height, speed):
         sprite.Sprite.__init__(self)
         self.image = transform.scale(image.load(player_image), (width, height))
         self.rect = self.image.get_rect()
+        self.rect.x = player_x
         self.rect.y = player_y
         self.speed = speed
     def reset(self):
@@ -44,13 +45,18 @@ class Player(GameSprite):
         if keys[K_DOWN] and self.rect.y < 605:
             self.rect.y += self.speed
 
-
-
+rackett_l = Player('racket.png', 5, 200, 25, 150, 15)
+rackett_r = Player('racket.png', 695, 200, 25, 150, 15)
+ball = GameSprite('ball.png', 320, 250, 50, 50, 10)
 
 while game:
     window.blit(background, (0, 0))
+    rackett_l.reset()
+    rackett_r.reset()
+    ball.reset()
     for e in event.get():
         if e.type == QUIT:
             game = False
     display.update()
     clock.tick(FPS)
+
