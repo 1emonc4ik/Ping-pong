a98b3938220efd97b74ce9c88d20d90f5dd518ef
DIO <110611144+1emonc4ik@users.noreply.github.com>
2022-08-16 18:36:09 +0500

    v.2

--- pin-pong.py
+++ pin-pong.py
@@ -2,9 +2,11 @@ from pygame import *
 from random import randint
 from time import time as timer
 
-window = display.set_mode((720, 500))
+win_wight = 720
+win_height = 500
+window = display.set_mode((win_wight, win_height))
 display.set_caption('Ping-Pong')
-background = transform.scale(image.load('bckg.jpg'), (1200, 720))
+background = transform.scale(image.load('bckg.jpg'), (win_wight, win_height))
 
 mixer.init()
 mixer.music.load('goldn.ogg')
@@ -14,9 +16,15 @@ game = True
 clock = time.Clock()
 FPS = 60
 life = 10
+finish = False
 
+speed_x = 3
+speed_y = 3
 font.init()
-font1 = font.SysFont('Arial', 40)
+win_1 = font.SysFont('Roboto', 40).render('win: Player_1', True, (255, 0, 0))
+lose_2 = font.SysFont('Roboto', 40).render('lose: Player_2', True, (255, 0, 0))
+win_2 = font.SysFont('Roboto', 40).render('win: Player_2', True, (255, 0, 0))
+lose_1 = font.SysFont('Roboto', 40).render('lose: Player_1', True, (255, 0, 0))
 lost = 0
 
 class GameSprite(sprite.Sprite):
@@ -33,30 +41,50 @@ class GameSprite(sprite.Sprite):
 class Player(GameSprite):
     def update_l(self):
         keys = key.get_pressed()
-        if keys[K_UP] and self.rect.y > 5:
+        if keys[K_w] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.y < 605:
+        if keys[K_s] and self.rect.y < 350:
             self.rect.y += self.speed
 
     def update_r(self):
         keys = key.get_pressed()
         if keys[K_UP] and self.rect.y > 5:
             self.rect.y -= self.speed
-        if keys[K_DOWN] and self.rect.y < 605:
+        if keys[K_DOWN] and self.rect.y < 350:
             self.rect.y += self.speed
 
 rackett_l = Player('racket.png', 5, 200, 25, 150, 15)
 rackett_r = Player('racket.png', 695, 200, 25, 150, 15)
-ball = GameSprite('ball.png', 320, 250, 50, 50, 10)
+ball = GameSprite('ball.png', 320, 250, 70, 70, 10)
 
 while game:
-    window.blit(background, (0, 0))
-    rackett_l.reset()
-    rackett_r.reset()
-    ball.reset()
-    for e in event.get():
-        if e.type == QUIT:
-            game = False
-    display.update()
-    clock.tick(FPS)
+    if finish != True:
+        window.blit(background, (0, 0))
+        rackett_l.reset()
+        rackett_l.update_l()
+        rackett_r.reset()
+        rackett_r.update_r()
+        ball.reset()
+        ball.rect.y -= speed_y
+        ball.rect.x -= speed_x
+        if sprite.collide_rect(ball, rackett_l) or sprite.collide_rect(ball, rackett_r):
+            speed_x *= -1
+        if ball.rect.y > win_height-50 or ball.rect.y < 0:
+            speed_y *= -1
+        if ball.rect.x < 0 :
+            finish = True
+            window.blit(win_2, (330 - 50, 300))
+            window.blit(lose_1, (330 - 50, 200))
+        
+        if ball.rect.x > win_wight :
+            finish = True
+            window.blit(win_1, (330 - 50, 300))
+            window.blit(lose_2, (330 - 50, 200))
+
+
+        for e in event.get():
+            if e.type == QUIT:
+                game = False
+        display.update()
+        clock.tick(FPS)
 
